85161cdcbec875197b40c85c635f625684a1da51
MiguelSequeiros <victor.sequeiros@ucsp.edu.pe>
2015-09-26 20:03:04 -0500

    Añadimos el archivo donde probamos nuestros algoritmos para la generación de claves

---
+++ test.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <string>
+
+
+bool isEven(int n)
+{
+	if(n&1 == 1)
+		return false;
+	return true;
+}
+
+int main()
+{
+	std::string strVallas("ABCDEFGHIJKLMN");
+	std::string str;
+	u_int key = 5;
+
+	u_int size = strVallas.size();
+	u_int contVallas = 0;
+
+	for(int i=0 ; i<key ; i++)
+		for(int j=0 ; j<size ; j++)
+			str += strVallas[(((i*size+j )*key) + i)%size];
+
+	for( int i=key-1 ; i<size ; i=i+2*key-2 )
+	{
+		//std::cout << strVallas[i] << "  con  " << str[size*(key-1)+i] << std::endl;
+		contVallas++;
+	}
+
+	//std::cout << "=======================================" << std::endl;
+
+	for( int i=key-1 ; i<size ; i=i+2*key-2 )
+	{
+		for(int j=1 ; j<key && i-j<size ; j++)
+		{
+			//std::cout << strVallas[i-j] << "  con  " << str[size*(key-1-j)+i-j] << std::endl;
+			contVallas++;
+		}
+
+	//std::cout << "=======================================" << std::endl;
+
+		for(int j=1 ; j<key && i+j<size; j++)
+		{
+			//std::cout << strVallas[i+j] << "  con  " << str[size*(key-1-j)+i+j] << std::endl;
+			contVallas++;
+		}
+
+	//std::cout << "=======================================" << std::endl;
+	}
+
+	if(contVallas<size)
+	{
+		u_int aux = size-contVallas;
+		for(int i=size-1 ; i>=contVallas ; i--)
+		{
+			//std::cout << strVallas[i] << "  con  " << str[aux*size+i] << std::endl;
+			aux--;
+		}
+	}
+
+
+	u_int last = 0;
+	for( int i=key-1 ; i<size ; i+=2*key-2 )
+	{
+		last = i ;
+		u_int currentPos = i;
+		bool sum = true;
+		char tmp;
+		int vueltas = 0;
+
+		for(int it = 0 ; vueltas < key/2 ; it=it+2)
+		{
+			for(int j=0 ; j<4 ; j++)
+			{
+				if( isEven(j) )
+				{
+					for( int k=0 ; k<key-1-it ; k++ )
+					{
+						//std::cout << currentPos << std::endl;
+						std::cout << str[currentPos] ;
+						currentPos = sum ? currentPos + size : currentPos - size ; 
+					}
+				}
+				else
+				{
+					for( int k=0 ; (i == key-1) ? (k<key-1-it) : (k<2*key-2-it -1) ; k++ )
+					{
+						//std::cout << currentPos << std::endl;
+						std::cout << str[currentPos] ;
+
+						if(i == key-1)
+							currentPos = sum ? (k==key-1-it -1 ? currentPos+size :currentPos+1) : currentPos-1 ;
+
+						else
+						{
+							//std::cout << "He entrado aqui" << std::endl;
+							currentPos = sum ? (( k==2*key-2-it-1 -1 ) ? currentPos+size : currentPos+1 ) : currentPos-1;
+						}
+
+					}
+				}
+				sum = (j==0 || j==1) ? false : true;  
+				std::cout << std::endl;
+			}
+			vueltas++;
+		}
+		//std::cout << currentPos << std::endl;
+
+		for(int j=0 ; i==key-1 ? j<1 : j<key-1 ; j++)
+			std::cout << str[currentPos--];
+
+		std::cout << std::endl;
+
+		std::cout << "=======================================" << std::endl;
+
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+/*
+	std::cout << last << std::endl;
+
+	if(last != size-1)
+	{
+		//std::cout << "Soy diferente" << std::endl;
+		int i = size-1 ;
+		u_int currentPos = i;
+		bool sum = true;
+		char tmp;
+		int vueltas = 0;
+
+		for(int it = 0 ; vueltas <(size-last-1)/2 ; it=it+2)
+		{
+			for(int j=0 ; j<4 ; j++)
+			{
+				if( isEven(j) )
+				{
+					for( int k=0 ; k<key-1-it ; k++ )
+					{
+						//std::cout << currentPos << std::endl;
+						std::cout << str[currentPos] ;
+						currentPos = sum ? currentPos + size : currentPos - size ; 
+					}
+				}
+				else
+				{
+					for( int k=0 ; k<size-1-last-1-it ; k++ )
+					{
+						//std::cout << currentPos << std::endl;
+						std::cout << str[currentPos] ;
+						currentPos = sum ? (( k==size-1-last -1-1 ) ? currentPos+size : currentPos+1 ) : currentPos-1;
+					}
+				}
+				sum = (j==0 || j==1) ? false : true;  
+				std::cout << "" << std::endl;
+			}
+			vueltas++;
+		}
+			std::cout << "=======================================" << std::endl;
+	}
+
+	
+*/
+
+	//std::cout << strMatriz << std::endl;
+	return 0;
+}
