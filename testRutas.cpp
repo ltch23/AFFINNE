6eded9bc4959a6e895e980ac7b9f9ca0f3d9ab4e
MiguelSequeiros <victor.sequeiros@ucsp.edu.pe>
2015-10-02 15:47:07 -0500

    Rutas casi acabado, solo falta manejar los corner cases

--- testRutas.cpp
+++ testRutas.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "Useful.h"
 
-const int key = 3;
+int key;
 
 void adjust_str(std::string & str)
 {
@@ -14,18 +14,79 @@ void adjust_str(std::string & str)
 
 int main()
 {
-	std::string str("DESCIFRANDO");
+	/*
+	std::cin>>key;
+	std::string str;
+	std::cin>>str;
+	*/
+
+	key = 5;
+	std::string str = "abcdefghijklmnopqrstuvwxy";
+
+	/* Ajustamos el string */
 
 	if( Useful::mod(str.size(),key) != 0)
 		adjust_str(str);
 
+
+	std::cout << str << std::endl;
+
+
+	/*creamos los limites*/
+	const int num_limit = str.size()/key-1;
+
+	int * limiters = new int[num_limit];
+
+	for(int i=1 ; i<=num_limit ; i++)
+	{
+		limiters[i-1] += (i)*key ;
+		//std::cout << limiters[i-1] << std::endl;
+	}
+
 	std::string str1;
 
+	/*RECORRIDO*/
+
+	bool block = true;
+	u_int cont = key;
+	u_int jump = 1;
+	bool toLeft = true;
+	u_int it=0;
+
 	for(int i=0 ; i<key ; i++)
-		for(int j=0 ; j<str.size() ; j++)
-			str1 += str[ (Useful::mod(((i*str.size()+j )*key)+i , str.size() ];
+	{
+		for(int j= (toLeft?1+it:num_limit-it) ; toLeft?(j<=num_limit-1-it):j>1+it ; toLeft?j++:j-- )
+		{
+			if(block)
+			{
+				for(int k=0 ; k<cont ; k++)
+				{
+					str1 += str[limiters[num_limit-j]+(toLeft?k+it:-k-1-it)];
+					std::cout << str1[str1.size()-1] << std::endl;
+				}
+				block = not block;
+				std::cout << "---------------------------------------" << std::endl;
+			}
+			str1 += str[limiters[num_limit-j]+(toLeft?-jump:jump-1)];
+
+			std::cout << str1[str1.size()-1] <<std::endl;
+
+		}
+		std::cout << "=======================================" << std::endl;
+		block = not block;
+		toLeft = not toLeft;
+
+		if(! Useful::isEven(i))
+		{
+			it++;
+			jump++;
+			cont-=2;
+		}
+	}
 
 
-	std::cout << str1 << std::endl;
+
+
+
 	return 0;
 }
